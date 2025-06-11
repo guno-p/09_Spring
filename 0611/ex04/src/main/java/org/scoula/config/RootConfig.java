7a3301590edb7b7e37883e56bc9cda53f9acd3c6
Guno <sushieatinggg@gmail.com>
2025-06-11 17:16:45 +0900

    폴더 구조 정리 + 250611 Spring DB 연동

---
+++ 0611/ex04/src/main/java/org/scoula/config/RootConfig.java
@@ -0,0 +1,70 @@
+package org.scoula.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+
+import javax.sql.DataSource;
+
+/**
+ * 🌱 Root Application Context 설정 클래스
+ * - Spring Framework의 최상위(Root) 애플리케이션 컨텍스트를 설정하는 클래스
+ * - 웹 계층과 무관한 비즈니스 로직, 서비스, 데이터 액세스 계층의 Bean들을 관리
+ */
+@Configuration
+@MapperScan(basePackages = {"org.scoula.mapper"})
+@PropertySource({"classpath:/application.properties"})
+public class RootConfig {
+    @Value("${jdbc.driver}") String driver;
+    @Value("${jdbc.url}") String url;
+    @Value("${jdbc.username}") String username;
+    @Value("${jdbc.password}") String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        HikariDataSource dataSource = new HikariDataSource(config);
+        return dataSource;
+    }
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:/mybatis-config.xml"));
+        sqlSessionFactory.setDataSource(dataSource());
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(){
+
+        DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource());
+        return manager;
+
+    }
+    // 현재는 기본 설정만 있는 상태
+    // 프로젝트 발전에 따라 다음과 같은 빈들을 추가할 수 있습니다:
+    // - 데이터베이스 설정
+    // - 트랜잭션 관리
+    // - 보안 설정
+    // - 외부 API 클라이언트
+    // - 캐시 설정
+    // - 스케줄링 설정
+}
---
+++ todolist0611/src/main/java/org/scoula/todo/service/TodoService.java
@@ -0,0 +1,15 @@
+package org.scoula.todo.service;
+
+import org.scoula.todo.domain.TodoDTO;
+
+import java.util.List;
+
+public interface TodoService {
+    List<TodoDTO> selectAll();
+
+    int insertTodo(TodoDTO todo);
+
+    int updateTodo(Long id);
+
+    int deleteTodo(Long id);
+}
